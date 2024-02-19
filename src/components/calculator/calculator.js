c75e10d271afa5c85f309d25a3659bb3a74253e1
ankita-tripathii <ankitatripathii011296@gmail.com>
2024-02-20 00:40:38 +0530

    add css in a calculator

--- src/components/calculator/calculator.js
+++ src/components/calculator/calculator.js
@@ -1,5 +1,5 @@
 import React from 'react'
-// import "./calculator.css"
+import "./calculator.css"
 import * as math from 'mathjs';
 
 export default function Calculator() {
@@ -9,27 +9,27 @@ export default function Calculator() {
         
         <div className="container" >
       <form >
-        <input type="text" />
+        <input type="text" value={result} />
       </form>
       <div className="keypad">
-        <button  id="clear" >Clear</button>
-        <button   id="backspace" >C</button>
-        <button  name="/"  >&divide;</button>
+        <button className="highlight" id="clear" >Clear</button>
+        <button className="highlight" id="backspace" >C</button>
+        <button className="highlight" name="/"  >&divide;</button>
         <button name="7"  >7</button>
         <button name="8"  >8</button>
         <button name="9"  >9</button>
-        <button  name="*"  >&times;</button>
+        <button className="highlight" name="*"  >&times;</button>
         <button name="4"  >4</button>
         <button name="5"  >5</button>
         <button name="6"  >6</button>
-        <button  name="-"  >&ndash;</button>
+        <button className="highlight" name="-"  >&ndash;</button>
         <button name="1"  >1</button>
         <button name="2"  >2</button>
         <button name="3"  >3</button>
-        <button  name="+"  >+</button>
+        <button className="highlight" name="+"  >+</button>
         <button name="0"  >0</button>
-        <button  name="."  >.</button>
-        <button  name="="  id="result" >=</button>
+        <button className="highlight" name="."  >.</button>
+        <button className="highlight" name="="  id="result" >=</button>
       </div>
     </div>
 
